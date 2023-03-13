99cc39d0e611ee83dec7aa5c318dfa821e3df05e
Akshay Parmar <akshay.parmar@priyasoft.com>
2023-03-13 14:16:34 +0530

    Server IP is changed

--- src/components/BoardUser.js
+++ src/components/BoardUser.js
@@ -1,40 +1,40 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 
 import UserService from "../services/user.service";
 import EventBus from "../common/EventBus";
 
 const BoardUser = () => {
-  const [content, setContent] = useState("");
+    const [content, setContent] = useState("");
 
-  useEffect(() => {
-    UserService.getUserBoard().then(
-      (response) => {
-        setContent(response.data);
-      },
-      (error) => {
-        const _content =
-          (error.response &&
-            error.response.data &&
-            error.response.data.message) ||
-          error.message ||
-          error.toString();
+    useEffect(() => {
+        UserService.getUserBoard().then(
+            (response) => {
+                setContent(response.data);
+            },
+            (error) => {
+                const _content =
+                    (error.response &&
+                        error.response.data &&
+                        error.response.data.message) ||
+                    error.message ||
+                    error.toString();
 
-        setContent(_content);
+                setContent(_content);
 
-        if (error.response && error.response.status === 401) {
-          EventBus.dispatch("logout");
-        }
-      }
+                if (error.response && error.response.status === 401) {
+                    EventBus.dispatch("logout");
+                }
+            }
+        );
+    }, []);
+
+    return (
+        <div className="container">
+            <header className="jumbotron">
+                <h3>{content}</h3>
+            </header>
+        </div>
     );
-  }, []);
-
-  return (
-    <div className="container">
-      <header className="jumbotron">
-        <h3>{content}</h3>
-      </header>
-    </div>
-  );
 };
 
 export default BoardUser;
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "http://localhost:8080",
+      target: "http://54.234.242.221:8080",
       changeOrigin: true,
     })
   );
